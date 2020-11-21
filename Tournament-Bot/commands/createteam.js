b169d17815a7f251470a6457c5cba97956022437
Nathan Jones <nathanskj@gmail.com>
2020-11-21 19:17:33 +0000

    Third Commit
    -Working on channel creation command.
    -teamcreation command partly working. Need to see about category id assigning

--- Tournament-Bot/commands/createteam.js
+++ Tournament-Bot/commands/createteam.js
@@ -3,14 +3,25 @@ module.exports = {
     
     name: 'createteam',
     description: 'Creates Team Role with specific permissions.',
+    args: true,
     execute(message, args) {
+        //let ownerRole = server.roles.get('776150055697514547');
+        //let moderatorRole = message.guild.roles.get('123456789');
         var server = message.guild;
 
+        if(message.member.roles.cache.some(r => r.name === "Mod")) {
+        //role creation
         server.roles.create({ data: { name: args[0], color: "c31414", permissions: ['CREATE_INSTANT_INVITE', 'CHANGE_NICKNAME', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY', 'SEND_MESSAGES', 'EMBED_LINKS'
         ,'ATTACH_FILES', 'USE_EXTERNAL_EMOJIS', 'CONNECT', 'SPEAK', 'USE_VAD'] } });
-
         message.channel.send('Team ' + args[0] + ' created!');
+        
+        //channel creation\
+        message.channel.send('Channel Created!');
+        server.channels.create(args[0] + ' text', "text");
 
+        }else{
+            message.channel.send('You do not have permission to do that');
+        }
 
 
         
--- Tournament-Bot/commands/twitch.js
+++ Tournament-Bot/commands/twitch.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'twitch',
     description: 'Sends Twitch Channel Link',
     execute(message, args) {
-        message.channel.send('This is our Twitch Channel Link: https://www.twitch.tv/vividtournaments');
+        message.channel.send('This is where our matches are streamed: https://www.twitch.tv/vividtournaments');
     },
 
 };
